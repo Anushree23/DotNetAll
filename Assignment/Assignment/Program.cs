7bdf8a9dea95292cf841d689b350235d0929a7f4
Anushree Agrawal <anushree.agrawal@zensar.com>
2020-08-04 22:05:08 +0530

    New file

---
+++ Assignment/Assignment/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Assignment
+{
+    abstract class Quadrilateral
+    {
+        int length;
+        int breadth;
+         public abstract int area( int length , int breadth);
+        public  Quadrilateral(int L , int B)
+        {
+            length = L;
+            breadth = B;
+        }
+    }
+
+    class Square : Quadrilateral
+    {
+        public Square(int L , int B) : base ( L , B  )
+        {
+
+        }
+        public override int area(int length , int breadth)
+        {
+            return length * length;
+        }
+        
+
+    }
+   
+
+    class Rectangle : Quadrilateral
+    {
+        int breadth;
+        public Rectangle(int L , int B) : base(L,B)
+        {
+
+        }
+      
+        public override int area(int length , int breadth)
+        {
+            return length * breadth ;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle R = new Rectangle(4,5);
+           R.area(4,5);
+            Console.WriteLine("area of rec= {0}" , R.area(4,5));
+          
+
+           Square S = new Square(4 , 0);
+            S.area(4 , 0);
+
+            Console.WriteLine("area of square= {0}", S.area(4,0));
+
+        }
+
+    }
+}
---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment3;
+namespace Assignment3
+{
+    class Program
+    {
+    
+        
+        static void Main(string[] args)
+        {
+            Vehicle veh = new Vehicle("Cielo" , "red",4,1,2);
+            veh.start();
+            veh.stop();
+            veh.Display();
+
+        }
+    }
+}
+
+
+
